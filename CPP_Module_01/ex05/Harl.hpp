31b0f14dd335da2bb67941c65e34bd77744fa9a0
p0tt3r-iOS <france998811@gmail.com>
2022-05-13 15:36:45 +0900

    Fix: CPP Module01 ex05: change message form

--- CPP_Module_01/ex05/Harl.hpp
+++ CPP_Module_01/ex05/Harl.hpp
@@ -3,10 +3,10 @@
 
 # include <string>
 
-# define DEBUG_MSG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
-# define INFO_MSG "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
-# define WARNING_MSG "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
-# define ERROR_MSG "This is unacceptable! I want to speak to the manager now."
+# define DEBUG_MSG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!\n"
+# define INFO_MSG "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger!\nIf you did, I wouldn’t be asking for more!\n"
+# define WARNING_MSG "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month.\n"
+# define ERROR_MSG "This is unacceptable! I want to speak to the manager now.\n"
 
 class Harl
 {
