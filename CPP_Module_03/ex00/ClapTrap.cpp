6e7272d9351d1020835f82df90978cba685f6b0f
p0tt3r-iOS <france998811@gmail.com>
2022-05-19 23:09:33 +0900

    Add: CPP Module 03 ex01 Add default constructor

--- CPP_Module_03/ex00/ClapTrap.cpp
+++ CPP_Module_03/ex00/ClapTrap.cpp
@@ -1,6 +1,10 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
+ClapTrap::ClapTrap() : _name("no_name"), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
 ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
     std::cout << "Default constructor called" << std::endl;
 }
