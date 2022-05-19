bd7ebef8d45346202d1ff385673a58e26f1e8310
p0tt3r-iOS <france998811@gmail.com>
2022-05-19 11:44:18 +0900

    Fix: CPP Module 02: ex03 func name(sign -> sine)

--- CPP_Module_02/ex03/bsp.cpp
+++ CPP_Module_02/ex03/bsp.cpp
@@ -1,7 +1,7 @@
 #include "Point.hpp"
 #include <iostream>
 
-float sign(Point const p1, Point const p2, Point const p3) {
+float sine(Point const p1, Point const p2, Point const p3) {
     return (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) - (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
 }
 
@@ -9,9 +9,9 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
     float   d1, d2, d3;
     bool    neg, pos;
 
-    d1 = sign(point, a, b);
-    d2 = sign(point, b, c);
-    d3 = sign(point, c, a);
+    d1 = sine(point, a, b);
+    d2 = sine(point, b, c);
+    d3 = sine(point, c, a);
 
     if (d1 == 0 || d2 == 0 || d3 == 0)
         return false;
