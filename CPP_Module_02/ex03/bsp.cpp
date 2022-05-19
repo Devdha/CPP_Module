abe6a6831b830b119415ecf3d9a5907fd9cd5eb1
p0tt3r-iOS <france998811@gmail.com>
2022-05-19 11:19:01 +0900

    Fix: CPP Module 02: ex03 After understanding

--- CPP_Module_02/ex03/bsp.cpp
+++ CPP_Module_02/ex03/bsp.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 
 float sign(Point const p1, Point const p2, Point const p3) {
-    bool result = (p1.getY() > ( p2.getY() - p3.getY() )/( p2.getX() - p3.getX() )*( p1.getX() - p3.getX() ) + p3.getY());
-    std::cout << result << '\n';
-    std::cout << p1.getX() << " - " << p3.getX() << " * " << p2.getY() << " - " << p3.getY() << ") - (" << p2.getX() << " - " << p3.getX() << " * " << p1.getY() << " - " << p3.getY() << std::endl; 
     return (p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) - (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
 }
 
@@ -15,8 +12,6 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
     d1 = sign(point, a, b);
     d2 = sign(point, b, c);
     d3 = sign(point, c, a);
-
-    std::cout << "test: " << d1 << " " << d2 << " " << d3 << std::endl;
 
     if (d1 == 0 || d2 == 0 || d3 == 0)
         return false;
