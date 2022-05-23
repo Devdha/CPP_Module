43763faed3fa4c13d210cf922a02c1839b1d7e49
p0tt3r-iOS <france998811@gmail.com>
2022-05-23 11:02:13 +0900

    Fix: CPP Module 03 ex02 Scav to Frag

--- CPP_Module_03/ex02/main.cpp
+++ CPP_Module_03/ex02/main.cpp
@@ -1,17 +1,16 @@
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main() {
-  ScavTrap A("A");
-  ScavTrap B("B");
-  ScavTrap C("C");
-  ScavTrap D;
+  FragTrap A("A");
+  FragTrap B("B");
+  FragTrap C("C");
+  FragTrap D;
 
   A.attack("B");
   B.takeDamage(5);
   B.takeDamage(5);
-  B.guardGate();
   B.beRepaired(10);
   C.beRepaired(5);
   D.attack("aaa");
-  D.guardGate();
+  D.highFiveGuys();
 }
