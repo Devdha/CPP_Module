0b14eb29594068a0401962bdfc4c5e01c872586f
p0tt3r-iOS <france998811@gmail.com>
2022-05-30 19:50:42 +0900

    Feat: Delete vscode setting.json

--- CPP_Module_05/ex02/main.cpp
+++ CPP_Module_05/ex02/main.cpp
@@ -2,31 +2,12 @@
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
-
-// void makeForm(std::string name, int req) {
-//   try {
-//     Form(name, req);
-//   } catch (std::exception& e) {
-//     std::cerr << "Fail to make Form " << name << " because " << e.what()
-//               << std::endl;
-//   }
-// }
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main() {
   Bureaucrat a("a", 150);
   Bureaucrat b("b", 1);
   Bureaucrat c("c", 75);
-
-  // Form as("af", 150);
-  // Form bs("bf", 1);
-
-  // c.signForm(as);
-  // c.signForm(bs);
-  // a.signForm(as);
-  // b.signForm(bs);
-  // c.signForm(as);
-  // c.signForm(bs);
-  // makeForm("test1", 151);
-  // makeForm("test2", 0);
-  // makeForm("test3", -15);
 }
