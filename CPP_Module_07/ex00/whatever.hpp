228efb885afb45de41af3d01c50dd22c2711387c
p0tt3r-iOS <france998811@gmail.com>
2022-06-04 17:50:16 +0900

    feat: done ex00

---
+++ CPP_Module_07/ex00/whatever.hpp
@@ -0,0 +1,22 @@
+#if !defined(WHATEVER_HPP)
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T& a, T& b) {
+  T tmp;
+  tmp = a;
+  a = b;
+  b = tmp;
+}
+
+template <typename T>
+const T& min(const T& a, const T& b) {
+  return a < b ? a : b;
+}
+
+template <typename T>
+const T& max(const T& a, const T& b) {
+  return a > b ? a : b;
+}
+
+#endif  // WHATEVER_HPP
