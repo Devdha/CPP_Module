77040bad9a09adb74b473c869fdace5710d830aa
p0tt3r-iOS <france998811@gmail.com>
2022-06-06 14:48:27 +0900

    feat: add test case

--- CPP_Module_08/ex01/main.cpp
+++ CPP_Module_08/ex01/main.cpp
@@ -25,4 +25,13 @@ int main() {
   s2.addNumber(124124124);
   std::cout << "S span: " << s2.shortestSpan() << std::endl;
   std::cout << "L span: " << s2.longestSpan() << std::endl;
+
+  Span sp = Span(5);
+  sp.addNumber(6);
+  sp.addNumber(3);
+  sp.addNumber(17);
+  sp.addNumber(9);
+  sp.addNumber(11);
+  std::cout << sp.shortestSpan() << std::endl;
+  std::cout << sp.longestSpan() << std::endl;
 }
